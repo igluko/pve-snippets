749130e3ee278ed97b5579fa457c01d87701ad1a
Budarov <45418632+Budarov@users.noreply.github.com>
2022-03-23 11:43:51 +0300

    Update change-lan.py

--- change-lan.py
+++ change-lan.py
@@ -5,67 +5,62 @@ import sys, os, json
 vmid = sys.argv[1]
 phase = sys.argv[2]
 hostname = os.uname()[1]
+conf_file = sys.path[0]+'/'+'change-lan.conf'
 
-if hostname.startswith("prod-prefix"):
+if hostname.startswith("pv"):
     print('State: ' + phase + ". Prod - exit.")
     sys.exit()
 
-prod_host = {
-    'name':'master-hostname',
-    'net0':'virtio=AA:AA:AA:AA:AA:AA,bridge=vmbr0,firewall=1',
-    'net1':'virtio=BB:BB:BB:BB:BB:BB,bridge=vmbr0,firewall=1,link_down=1'
-    }
-
-reserve_host = {
-    'name':'slave-hostname',
-    'net0':'virtio=AA:AA:AA:AA:AA:AA,bridge=vmbr0,firewall=1,link_down=1',
-    'net1':'virtio=BB:BB:BB:BB:BB:BB,bridge=vmbr0,firewall=1'
-    }
-
-def check_conf(prod_host, reserve_host, hostname):
-    
-    if hostname == prod_host['name']:
-        print('Start on prod server: ' + prod_host['name'] )
-        return('prod')
-    elif hostname == reserve_host['name']:
-        print('Start on reserve server: ' + reserve_host['name'])
-        return('reserve')
+def get_conf(file):
+    if os.path.isfile(file):
+        with open(file,'r') as conf_file:
+            try:
+                data = json.load(conf_file)
+            except json.decoder.JSONDecodeError:
+                sys.exit('Not valid json in file ' + file)
+        conf_file.close()
+        return(data['conf'])
     else:
-         sys.exit('Start on unknown host: hostname')
-    
-def set_new_conf(from_host, to_host):
+        sys.exit('Conf file ' + file + ' not found')
+
+
+def find_conf(configs, vmid, hostname):
+    result = []
+    for config in configs:
+        if (config['Hostname'] == hostname) and (str(config['VMID']) == vmid):
+            result = config
+    if len(result) == 0:
+        sys.exit('Not found config for ' + hostname + ':' + vmid)
+    return(result)
+            
+def check_mac(new_conf, vmid, hostname):
     
     cmd = 'pvesh get /nodes/' + hostname + '/qemu/' + vmid + '/config -output-format json'
-    conf = json.load(os.popen(cmd)) 
-    
-    res_conf = []
-    
-    for k, v in to_host.items():
+    conf_vm = json.load(os.popen(cmd))
+    net_conf_vm = ''
+    for k, v in conf_vm.items():
         if 'net' in k:
-            if conf[k] == v:
-                print(k + ' conf is OK')
-            elif conf[k] == from_host[k]:
-                print('Configuration ' + k + ' needs to be changed.')
-                res_conf.append("sed -i 's/" + from_host[k] + '/' + to_host[k] + "/'  /etc/pve/qemu-server/" + vmid + '.conf')
-            else:
-                sys.exit('Unknown configuration for ' + k + ' on host')
+            net_conf_vm += v   
+    if (new_conf['MAC_UP'] in net_conf_vm) and (new_conf['MAC_DOWN'] in net_conf_vm):
+        pass
+    else:
+        sys.exit('Does not match MAC address')
+
+def get_bash_conf(new_conf):
+    res_conf = []
+    res_conf.append('sed -i -r "s/(net.: virtio=)(' + new_conf['MAC_UP'] + ')(.*)(,link_down=1)/\\1\\2\\3/" /etc/pve/local/qemu-server/' + str(new_conf['VMID']) + '.conf')
+    res_conf.append('sed -i -r "/,link_down=1/! s/(net.: virtio=)(' + new_conf['MAC_DOWN'] + ')(.*)/\\1\\2\\3,link_down=1/" /etc/pve/local/qemu-server/' + str(new_conf['VMID']) + '.conf')
     return(res_conf)
 
-
 if phase == 'pre-start':
-
-   print('Snippet started: pre-start')
-
-   host_status = check_conf(prod_host, reserve_host, hostname)
-
-   if host_status == 'prod':
-       cmd_list = set_new_conf(reserve_host, prod_host)
-   elif host_status == 'reserve':
-       cmd_list = set_new_conf(prod_host, reserve_host)
-
-   for cmd in cmd_list:
-       print('Command to execute: ' + cmd)
-       status = os.system(cmd)
-       if status != 0:
-           sys.exit('The command completed with an error: ' + status)
-   print('Snippet work done')
+    print('Snippet started: pre-start')
+    configs = get_conf(conf_file)
+    new_conf = find_conf(configs, vmid, hostname)
+    check_mac(new_conf, vmid, hostname)
+    cmd_list = get_bash_conf(new_conf)
+    for cmd in cmd_list:
+        print('Command to execute: ' + cmd)
+        status = os.system(cmd)
+        if status != 0:
+            sys.exit('The command completed with an error: ' + status)
+    print('Snippet work done')
